6b3b618ccd7fc4c9e240b3562cfbe1456eaca082
Sofiamishel2003 <sofiamishel2003@gmail.com>
2026-03-12 17:56:11 -0600

    Feat: Generación de Claves

--- generar_claves.py
+++ generar_claves.py
@@ -1,9 +1,19 @@
 from Crypto.PublicKey import RSA
 
 def generar_par_claves(bits: int = 3072):
-    # TODO: implementar con RSA.generate()
-    # Recordar: mínimo 2048 bits; recomendado 3072 para nuevo software
-    pass
+    key = RSA.generate(bits)
+    private_key = key.export_key(
+        format="PEM",
+        passphrase="lab04uvg"
+    )
+    public_key = key.publickey().export_key()
+
+    # Guardar las claves en archivos
+    with open("private_key.pem", "wb") as f:
+        f.write(private_key)
+
+    with open("public_key.pem", "wb") as f:
+        f.write(public_key)
 
 if __name__ == '__main__':
     generar_par_claves(3072)
