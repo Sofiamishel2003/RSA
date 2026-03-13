944aedd3296f062fa105fe80d0a24d53180a6bf4
Sofiamishel2003 <sofiamishel2003@gmail.com>
2026-03-12 18:02:51 -0600

    feat: Cifrado y Decifrado con RSA-OAEP

--- rsa_OAEP.py
+++ rsa_OAEP.py
@@ -2,12 +2,16 @@ from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
 def cifrar_con_rsa(mensaje: bytes, public_key_pem: bytes) -> bytes:
-    # TODO: implementar
-    pass
+    key = RSA.import_key(public_key_pem)
+    cipher = PKCS1_OAEP.new(key)
+    cifrado = cipher.encrypt(mensaje)
+    return cifrado
 
 def descifrar_con_rsa(cifrado: bytes, private_key_pem: bytes) -> bytes:
-    # TODO: implementar
-    pass
+    key = RSA.import_key(private_key_pem, passphrase="lab04uvg")
+    cipher = PKCS1_OAEP.new(key)
+    descifrado = cipher.decrypt(cifrado)
+    return descifrado
 
 if __name__ == '__main__':
     # Cargar claves generadas en el ejercicio anterior
@@ -28,5 +32,7 @@ if __name__ == '__main__':
     # Pregunta: ¿Por qué cifrar el mismo mensaje dos veces produce
     # resultados distintos? Demuéstralo y explica qué propiedad de OAEP lo causa.
     c1 = cifrar_con_rsa(mensaje_original, pub)
+    print("Cifrado 1: ", c1.hex()[:64], "...")  # primeros 64 chars hex
     c2 = cifrar_con_rsa(mensaje_original, pub)
+    print("Cifrado 2: ", c2.hex()[:64], "...")  # primeros 64 chars hex
     print(f"\nc1 == c2: {c1 == c2}")   # Esperado: False
